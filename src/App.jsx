8365495fe9da5398b4f7aca41596e49f4e2867dd
HPhucJ2007 <yourgitemail@example.com>
2022-09-08 18:33:59 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import GlobalStyles from './components/GlobalStyles';
+
+function App() {
+    return (
+        <div className="app">
+            <GlobalStyles>
+                <h1>hi</h1>
+            </GlobalStyles>
+        </div>
+    );
+}
+
+export default App;
